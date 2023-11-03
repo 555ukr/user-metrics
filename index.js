42158f90085219f1e9749b9b105d8291cc9fc763
Dmytro.Kalashnyk <dmytro.kalashnyk@sinch.com>
2023-11-03 17:23:46 +0100

    push rate to ga

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
 var cron = require('node-cron');
+const axios = require('axios');
+// const ga4mp = require('ga4-mp');
+const http = require('http')
+const fs = require('fs')
 
-//fetch infor from https://bank.gov.ua/NBU_Exchange/exchange_site?start=20231101&end=20231101&valcode=usd&json
+const url = "https://bank.gov.ua/NBU_Exchange/exchange_site?start=20231101&end=20231101&valcode=usd&json"
 
-cron.schedule('0 * * * *', () => {
-  console.log('running a task every minute');
+const secretKey = "oBce26vwSl2Et36wWklnDQ";
+const clientId = "710475967.1699020165";
+const measurementId = "G-FT32LKB4ZZ"
+
+const pushMetric = async (data) => {
+  // let ga4 = ga4mp.createClient(secretKey, measurementId, clientId);
+  // const result = ga4.send(events.map(rate => ({...rate, name: "usd_uah_rate"})));
+  const result = await axios.post(`https://www.google-analytics.com/mp/collect?api_secret=${secretKey}&measurement_id=${measurementId}`,
+      {
+        "client_id": clientId,
+        // "timestamp_micros":"1699026648907745",
+        // "non_personalized_ads":true,
+        "events": data.map(rate => ({
+            name: "usd_uah_rate",
+            params: rate
+        }))
+      })
+    console.log({
+        code: result.status,
+        response: result.data
+    })
+}
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+cron.schedule('*/10 * * * * *', async () => {
+  const result = await axios.get(url);
+  if (result.status == 200){
+    pushMetric(result.data)
+  }
 });
+
+server.listen(process.env.PORT || 3000)
